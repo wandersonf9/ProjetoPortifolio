e5914a8db9f8fce2382b98c9e701a31c0042341a
wanderson francisco <wandersonarthur906@gmail.com>
2024-04-28 03:04:53 -0300

    subindo projeto

---
+++ Menu.js
@@ -0,0 +1,13 @@
+const menuMobile= document.getElementById('menu-mobile');
+const menuList= document.getElementById('btn-mobile');
+
+menuMobile.addEventListener('click', animarMenu)
+
+function animarMenu(){
+    menuMobile.classList.toggle('abrir')
+    menuList.classList.toggle('ativo')
+}
+
+
+
+
