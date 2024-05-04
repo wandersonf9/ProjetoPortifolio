4178a3eab3824612344024c0d9218be79ad01220
wanderson francisco <wandersonarthur906@gmail.com>
2024-05-03 23:57:43 -0300

    subindo projeto

--- Menu.js
+++ Menu.js
@@ -1,11 +1,10 @@
 const menuMobile= document.getElementById('menu-mobile');
 const menuList= document.getElementById('btn-mobile');
 
-menuMobile.addEventListener('mouseover', animarMenu)
+menuMobile.addEventListener('click', animarMenu)
 
 function animarMenu(){
     menuMobile.classList.toggle('abrir')
-    menuList.classList.toggle('ativo')
 }
 
 
---
+++ formulario/form.js
@@ -0,0 +1,48 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  const passwordConfirmationValue = passwordConfirmation.value;
+
+  if (usernameValue === "") {
+    setErrorFor(username, "O nome de usuário é obrigatório.");
+  } else {
+    setSuccessFor(username);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "O email é obrigatório.");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um email válido.");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "A senha é obrigatória.");
+  } else if (passwordValue.length < 7) {
+    setErrorFor(password, "A senha precisa ter no mínimo 7 caracteres.");
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (passwordConfirmationValue === "") {
+    setErrorFor(passwordConfirmation, "A confirmação de senha é obrigatória.");
+  } else if (passwordConfirmationValue !== passwordValue) {
+    setErrorFor(passwordConfirmation, "As senhas não conferem.");
+  } else {
+    setSuccessFor(passwordConfirmation);
+  }
+}
